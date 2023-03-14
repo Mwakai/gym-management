91447b661147b8db8ed845900c204548a89931d0
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-03-14 22:29:54 +0300

    members admin layout

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -7,4 +7,8 @@ use Illuminate\Http\Request;
 class MembersController extends Controller
 {
     //
+    public function addMembers()
+    {
+        return view('members.index');
+    }
 }
---
+++ resources/views/admin/members.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.backend')
+@section('content')
+
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary collapsed-card">
+                <div class="card-header">
+                    <h3 class="card-title">
+                        <b>Members</b>
+                    </h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-plus"></i>
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+@endsection
+
