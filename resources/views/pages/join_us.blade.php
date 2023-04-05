f7932ae429f565e701f2e715ed88a52197228781
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-04-05 23:09:12 +0300

    ui

--- resources/views/pages/join_us.blade.php
+++ resources/views/pages/join_us.blade.php
@@ -80,19 +80,19 @@ span.price {
 </style>
 
 <div class="container">
-<script>  
-  (function(d, s, id){
-     var js, mpesa = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/src/button.min.js";
-     mpesa.parentNode.insertBefore(js, mpesa);
-   }(document, 'script', 'mpesa_btn_js'));
-</script>
+    <script>  
+      (function(d, s, id){
+        var js, mpesa = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/src/button.min.js";
+        mpesa.parentNode.insertBefore(js, mpesa);
+      }(document, 'script', 'mpesa_btn_js'));
+    </script>
     <div class="row">
     <div class="col-75">
         <div class="container">
-        <form action="/action_page.php">
+        <form action="">
 
             <div class="row">
             <div class="col-50">
