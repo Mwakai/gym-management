2590ecb398a7f57e97c11c26e8f5ecfff7791f68
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-04-18 23:52:34 +0300

    mpesa intergration

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -30,6 +30,7 @@ class MembersController extends Controller
             'name' => 'required',
             'email' => 'required',
             'phone' => 'required',
+            'payment' => 'required',
 
         ]);
 
@@ -37,7 +38,35 @@ class MembersController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'phone' => $request->phone,
+            'payment' => $request->payment,
         ]);
+        
+        $BusinessShortCode = '174379';
+        $LipaNaMpesaPasskey = 'e2d3b0e1b1e1b1e1b1e1b1e1b1e1b1e1';
+        $TransactionType = 'CustomerPayBillOnline';
+        $Amount = $request->payment;
+        $PartyA = $request->phone;
+        $PartyB = '174379';
+        $PhoneNumber = $request->phone;
+        $CallBackURL = 'https://safaricom.co.ke/mpesa_online/lnmo_checkout_server.php?wsdl';
+        $AccountReference = 'Mpesa';
+        $TransactionDesc = 'Mpesa';
+        $Remarks = 'Mpesa';
+
+        $mpesa= new Mpesa();
+        $stkPushSimulation = $mpesa->STKPushSimulation(
+            $BusinessShortCode, // This is the paybill number
+            $LipaNaMpesaPasskey, // Lipa Na Mpesa Online Passkey
+            $TransactionType, // Transaction type CustomerPayBillOnline
+            $Amount, // The amount the customer is paying
+            $PartyA, // The phone number of the customer
+            $PartyB, // The organization shortcode used to receive the transaction.
+            $PhoneNumber, // Phone number same as $PartyA
+            $CallBackURL, // The url to where responses from M-Pesa will be sent to
+            $AccountReference, // Used with M-Pesa PayBills.
+            $TransactionDesc, // A description of the transaction.
+            $Remarks, // Comments that are sent along with the transaction.
+        );
 
         return redirect()->back()->with('success', 'Member Added Successfully');
 
