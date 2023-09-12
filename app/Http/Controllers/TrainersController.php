76241bc40adf8351e33a3cc62388dc9e715a7904
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-09-13 02:22:50 +0300

    trainers functions

--- app/Http/Controllers/TrainersController.php
+++ app/Http/Controllers/TrainersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trainer;
 use Illuminate\Http\Request;
 
 class TrainersController extends Controller
@@ -10,5 +11,21 @@ class TrainersController extends Controller
 
     public function index() {
         return view('admin.trainers');
+    }
+
+    public function addTrainer() {
+
+    }
+
+    public function updateTrainer() {
+
+    }
+
+    public function deleteTrainer(Request $request) {
+        $id = $request->id;
+        Trainer::where('id', $id)->delete();
+
+        return redirect()->back()->with('success', 'Trainer deleted successfully');
+
     }
 }
