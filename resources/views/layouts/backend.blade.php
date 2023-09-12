8fe45c0cc7c28acab4222567eb7a8909c60059eb
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-09-13 01:49:59 +0300

    cleanup

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -157,24 +157,9 @@
             </svg>
             <p>
               Trainers
-              <i class="fas fa-angle-left right"></i>
             </p>
           </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p></p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p></p>
-              </a>
-            </li>
-            
-          </ul>
+          
         </li>
 
         <li class="nav-header">ADMINS</li>
