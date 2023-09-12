7342fc22929ea4bdd2eca920b8eea80d5195f84e
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-09-13 02:03:09 +0300

    route to admin.trainers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers;
 use App\Http\Controllers\MembersController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TrainersController;
 
 
 /*
