f166f622322217dd37cfec119234c582c5cd3bd4
Alvin Mwakai <mwakaimwambala@gmail.com>
2023-09-13 22:37:37 +0300

    add trainers

--- app/Http/Controllers/TrainersController.php
+++ app/Http/Controllers/TrainersController.php
@@ -13,7 +13,25 @@ class TrainersController extends Controller
         return view('admin.trainers');
     }
 
-    public function addTrainer() {
+    public function addTrainer(Request $request) {
+        $request->validate([
+            'name' => 'required'|'string'|'min:3'|'max:255',
+            'email' => 'required'|'string'|'email',
+            'phone' => 'required'|'string'|'min:10'|'max:10',
+            'image' => 'required'|'image'|'mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $image=time().'.'.$request->image->extension();
+        $request->image->move(public_path('images'), $image);
+
+        Trainer::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'image' => $image
+        ]);
+
+        return redirect()->back()->with('success', 'Trainer added successfully');
 
     }
 
